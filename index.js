5749d28d32acdf270bb8afe097f306c0d928686d
davide <d.bulbarelli@aliaslab.net>
2019-11-13 19:06:21 +0100

    ciao mondino 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Ciao Mondino!');
+  res.send('Ciao Mondino 2!');
 });
 
 app.listen(8080, function () {
