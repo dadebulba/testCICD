c044097d463059972700584a7d6f60f6ae8f1517
davide <d.bulbarelli@aliaslab.net>
2019-11-14 14:44:03 +0100

    added prom-client

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 var express = require('express');
+const client = require('prom-client');
 var app = express();
 
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+// Probe every 5th second.
+collectDefaultMetrics({ timeout: 3000 });
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World prometheus!');
 });
 
 app.listen(8080, function () {
